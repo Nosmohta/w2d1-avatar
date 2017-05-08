c8248f9cfeba591f2a8b8a1adee442e2a40ab402
Andrew Thomson <Thomson.w.andrew@gmail.com>
2017-05-08 21:43:41 +0000

    adding requestURL information and string generation.

--- download-avatars.js
+++ download-avatars.js
@@ -1,10 +1,13 @@
-var request = require("request");
-
-
 
 function getRepoContributors(repoOwner, repoName, cb){
 
+var request = require("request");
+var GITHUB_USER = "Nosmohta";
+var GITHUB_TOKEN = "614ba4b68f00f8095735e872add3368d9072a0cb";
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
 console.log('Welcome to the GitHub Avatar Downloader!');
+console.log( requestURL);
 
 
 
