0206bb93d3abd6f7b863ebdab0f16ca5c6a22c7f
Andrew Thomson <Thomson.w.andrew@gmail.com>
2017-05-09 01:34:19 +0000

    download images function created and unit tested.

--- download-avatars.js
+++ download-avatars.js
@@ -1,44 +1,49 @@
+ var request = require("request");
+ var fs = require( "fs");
 
 
 function getRepoContributors(repoOwner, repoName, cb){
-
-  var request = require("request");
   var GITHUB_USER = "Nosmohta";
   var GITHUB_TOKEN = "614ba4b68f00f8095735e872add3368d9072a0cb";
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
-  console.log( requestURL);
 
   const requestOptions = {
     url: requestURL,
     headers: {
       "user-agent": "GitHub Avatar Downloader - Student Project"
     }
-  }
-
+  };
 
   request.get(requestOptions, (error, response, body) => {
     if (error) {
       console.log( "Error: ", err);
       return
     }
-
-      let contributors = JSON.parse(body);
-      console.log(contributors);
-      contributors.map( (x) => {
-        console.log(x.avatar_url);
-
-      })
-
-  });
+    let contributors = JSON.parse(body);
+    let avatarURLs = [];
+    contributors.map( (x) => avatarURLs.push( x.avatar_url));
+    console.log(avatarURLs);
+  })
 
 }
 
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on( "error", (err) => {
+      if (err) {
+        console.log( "error :", err)
+      }
+    })
+    .pipe(fs.createWriteStream(filePath));
+}
+
+
+downloadImageByURL('https://avatars2.githubusercontent.com/u/43004?v=3', "./images/test.jpg" )
 
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+// });
