aa2da84f288e648810056aebf75c356702b666f4
Andrew Thomson <Thomson.w.andrew@gmail.com>
2017-05-09 00:02:47 +0000

    request functionality added with user-agent defined.

--- download-avatars.js
+++ download-avatars.js
@@ -6,8 +6,47 @@ var GITHUB_USER = "Nosmohta";
 var GITHUB_TOKEN = "614ba4b68f00f8095735e872add3368d9072a0cb";
 var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
-console.log('Welcome to the GitHub Avatar Downloader!');
 console.log( requestURL);
+
+const requestOptions = {
+  url: requestURL,
+  headers: {
+    "user-agent": "GitHub Avatar Downloader - Student Project"
+  }
+
+
+
+
+}
+
+
+
+// enter a user-agent: Value for the request? "GitHub Avatar Downloader - Student Project"
+
+request.get(requestOptions, (error, response, body) => {
+  if (error) {
+    console.log( "Error: ", err);
+  }
+
+    let json = JSON.parse(body);
+    console.log(json);
+
+
+
+})
+
+
+
+       // .on( "error", (err) => {
+       //    console.log( "Error: ", err);
+       // })
+       // .on( "response", (data) => {
+       //    console.log( "head : ", data.head)
+       //    //console.log( data);
+
+       // });
+
+
 
 
 
