9d2e9c8b7ea772a57bd4d85b8a482a9ffe20954c
Andrew Thomson <Thomson.w.andrew@gmail.com>
2017-05-09 02:32:54 +0000

    cosmetic code touch-up and moving argument logic check to main function

--- download-avatars.js
+++ download-avatars.js
@@ -1,41 +1,34 @@
- var request = require("request");
- var fs = require( "fs");
+const request = require("request");
+const fs = require( "fs");
+const args = process.argv.slice(2);
 
 
-let args = process.argv.slice(2)
-if(!(args[0]&&args[1])) {
+function getRepoContributors(repoOwner, repoName) {
+  const GITHUB_USER = "Nosmohta";
+  const GITHUB_TOKEN = "614ba4b68f00f8095735e872add3368d9072a0cb";
+  const requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  const requestOptions = {
+    url: requestURL,
+    headers: { "user-agent": "GitHub Avatar Downloader - Student Project"}
+  };
+
+  if(!(repoOwner||repoName)) {
    console.log("Please provide two valid arguments.");
    console.log("Eg. : node downloaded-avatars.js jquery jquery");
    return;
   }
 
-console.log(args);
-
-function getRepoContributors(repoOwner, repoName){
-
-  const GITHUB_USER = "Nosmohta";
-  const GITHUB_TOKEN = "614ba4b68f00f8095735e872add3368d9072a0cb";
-  const requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + args[0] + '/' + args[1] + '/contributors';
-
-  const requestOptions = {
-    url: requestURL,
-    headers: {
-      "user-agent": "GitHub Avatar Downloader - Student Project"
-    }
-  };
-
   request.get(requestOptions, (error, response, body) => {
     if (error) {
       console.log( "Error: ", err);
-      return
+      return;
     }
-    let contributors = JSON.parse(body);
     let avatarURLs = [];
-    contributors = contributors.map( (x) => avatarURLs.push( x.avatar_url))
+    let contributors = JSON.parse(body).map( (x) => avatarURLs.push( x.avatar_url))
     avatarURLs.map( ( x, i) => {
       downloadImageByURL( x , "./images/avatar" + i + ".jpg");
     })
-  })
+  });
 }
 
 
@@ -43,11 +36,13 @@ function downloadImageByURL(url, filePath) {
   request.get(url)
     .on( "error", (err) => {
       if (err) {
-        console.log( "error :", err)
+        console.log( "error :", err);
+        return;
       }
     })
     .pipe(fs.createWriteStream(filePath));
 }
+
 
 getRepoContributors(...args);
 
